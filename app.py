0107e853d1f3f47fb42cb05ee6948ca0f5b2e626
hawkilin <77032981+hawkilin@users.noreply.github.com>
2021-08-12 10:39:32 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import flask
+from flask import request
+import requests
+from bs4 import BeautifulSoup
+from wiktionaryparser import WiktionaryParser
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET', 'POST'])
+def wikipedia_scraper():
+    term = request.args.get('term')
+    response = requests.get(url=f"https://en.wikipedia.org/wiki/{term}",)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    allp = soup.find(id="bodyContent").find_all("p")
+    paragraph = allp[1].text
+    return paragraph
+
+
+
+@app.route('/wiktionary', methods=['GET', 'POST'])
+def wiktionary_scraper():
+    term = request.args.get('term')
+    parser = WiktionaryParser()
+    word = parser.fetch(term)
+    definition = word[0]['definitions']
+    definition2 = definition[0]['text']
+    definition3 = definition2[1:]
+    return definition3
+
+
+app.run()
